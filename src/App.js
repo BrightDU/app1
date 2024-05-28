49e566a9652cd28e0aa783696e4ea8044d911994
BrightDU <brightuto@gmail.com>
2024-05-28 14:19:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './styles/App.css';
+import SmartButton from './components/smartButton';
+//import ResetButton  from './components/resetButton';
+
+
+
+function App() {
+  
+    return (
+      <div className="App">
+        <h1>Smart Button App</h1>
+        <SmartButton />
+        
+  
+      </div>
+    );
+  
+    };
+
+  
+
+export default App;
